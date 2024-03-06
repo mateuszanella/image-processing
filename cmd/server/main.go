5b99cb4a2de75fd9b86ac4288ae034ffe9a7514d
mateuszanella <mateusblk1@gmail.com>
2024-03-05 21:02:36 -0300

    mudanças positivas

---
+++ cmd/server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
+func main() {
+
+	tmpl := template.Must(template.ParseFiles("internal/templates/index.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmpl.Execute(w, data)
+	})
+	http.ListenAndServe(":80", nil)
+
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
+
+	// http.ListenAndServe(":8080", nil)
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    'internal/templates/**/*.templ',
+    'internal/templates/**/*.html',
+  ],
   theme: {
     extend: {},
   },
